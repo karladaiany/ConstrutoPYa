0d5d73c5e3609494853e29660b70885ff7c68c5f
karladaiany <karllinhadeoliveira@live.com>
2021-09-27 22:46:58 -0300

    Atualização a partir da aula 11. Uso do @pytest.mark.parametrize; try_except; teste positivo e teste negativo.

--- main.py
+++ main.py
@@ -18,18 +18,29 @@ def subtrair(num1, num2):
 def multiplicar(num1, num2):
     return num1 * num2
 
+# exemplo de demonstração
 def dividir(num1, num2):
     if num2 != 0:
         return num1 / num2
     else:
         return 'Não dividirás por zero'
 
+# exemplo de divisão correto
+def dividir_try_except(num1, num2):
+    try:
+        return num1 / num2
+    except:
+        return 'Não dividirás por zero'
+    #except TypeError:
+        #return TypeError
+
 if __name__ == '__main__':
     print_hi('KarlaDaiany')
 
     # soma de 2 números
-    resultado = somar(1,2)
+    resultado = somar(2,4)
     print(f'O resultado da soma é: {resultado}')
+
 
     #subtração de 2 números
     resultado = subtrair(5,3)
@@ -39,9 +50,12 @@ if __name__ == '__main__':
     resultado = multiplicar(2,4)
     print(f'O resultado da multiplicação é: {resultado}')
 
-    #divisão
+    #divisão demonstração
     resultado = dividir(9,0)
     print(f'O resultado da divisão é: {resultado}')
+
+    #divisão correta
+
 
 
 
@@ -61,8 +75,18 @@ def test_somar_didatico():
     # 3 - Check / Valida
     assert resultado_atual == resultado_esperado
 
-def test_somar():
-    assert somar(8,5) == 13
+@pytest.mark.parametrize('num1,num2,resultado',[
+        # valores
+        (5, 4, 9),  # teste 1
+        (3, 2, 5),  # teste 2
+        (10, 6, 15), # teste 3
+    ])
+def test_somar(num1, num2, resultado):
+    try:
+        assert somar(num1,num2) == resultado
+    except AssertionError:
+        print(f'Entrou no Except: {AssertionError}')
+        pass
 
 def test_subtrair():
     assert subtrair(4,5) == -1
@@ -72,3 +96,18 @@ def test_multiplicar():
 
 def test_dividir():
     assert dividir(8,4) == 2
+
+def test_dividir_por_zero():
+    assert dividir(8,0) == 'Não dividirás por zero'
+
+    # teste positivo => mostra o resultado correto
+    #                => avança para a próxima etapa
+    # teste negativo => mostra a mensagem de erro (resultado_esperado do teste negativo)
+
+@pytest.mark.parametrize('num1, num2, resultado',[
+    (8,2,4),
+    (20,4,5),
+    (10,0,'Não dividirás por zero')
+])
+def test_dividir_try_except_por_zero(num1,num2,resultado):
+    assert dividir_try_except(num1,num2) == resultado
